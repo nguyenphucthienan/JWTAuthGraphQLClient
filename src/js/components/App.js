3c4733439d4648e12089a076f58aac412d723c7d
Nguyen Phuc Thien An <npta97@gmail.com>
2018-01-30 08:40:02 +0700

    Add React-Router
    Add Landing
    Update Header

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import Header from './Header';
-import LoginForm from './LoginForm';
 import Footer from './Footer';
+import Landing from './Landing';
+import LoginForm from './LoginForm';
 
 class App extends Component {
   render() {
     return (
-      <div className="flex-wrapper">
-        <Header />
-        <main>
-          <LoginForm />
-        </main>
-        <Footer />
-      </div>
+      <HashRouter>
+        <div className="flex-wrapper">
+          <Header />
+          <main>
+            <Switch>
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/login" component={LoginForm} />
+            </Switch>
+          </main>
+          <Footer />
+        </div>
+      </HashRouter>
     );
   }
 }
--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderButtons() {
+    return (
+      <div>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Register</Link></li>
+      </div>
+    );
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper blue darken-1">
-          <a href="#" className="brand-logo left">
+          <Link to="/" className="brand-logo">
             <i className="material-icons">home</i>Auth
+          </Link>
+
+          <a data-activates="nav-mobile" className="button-collapse pointer-cursor">
+            <i className="material-icons">menu</i>
           </a>
+          <ul className="right hide-on-med-and-down">
+            {this.renderButtons()}
+          </ul>
+          <ul className="side-nav" id="nav-mobile">
+            {this.renderButtons()}
+          </ul>
         </div>
       </nav>
     );
